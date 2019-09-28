6cb88d3b9619a62bb8e54b3deb46550c216ddad4
Kevin Zhou <kjzhou2@illinois.edu>
2019-09-28 13:05:03 -0500

    webscraping update and adding UrbanaEvents

--- webScraping.py
+++ webScraping.py
@@ -11,6 +11,67 @@ if __name__ == "__main__":
     location = []
     info = []
     contactInfo = []
+    description = []
+    emptyDesc = []
+    emptyUpvotes = []
+    content = driver.get("https://calendars.illinois.edu/list/7")
+    info = (driver.find_element_by_class_name('place-on-screen')).text.split("\n")
+    print(info[0])
+
+    currentDate = "Saturday"
+    formattedDate = "month dd, yyyy"
+    for i in range(0, len(info) - 4):
+        try:
+
+            if (info[i].split(',')[2] == ' 2019'):
+                currentDate = info[i]
+                formattedDate = currentDate.split(',')[1].strip() + "," + currentDate.split(',')[2]
+        except IndexError:
+
+            information = info[i]
+            try:
+                #checks for times
+                if (info[i] == "All Day"):
+                    date.append(formattedDate)
+                    times.append("12:00am - 11:59pm")
+                    location.append((info[i + 1]))
+                    eventName.append((info[i + 2]))
+                    description.append((info[i + 3]))
+                #checks that there is a time
+                elif (isinstance(int(information[0:1]), int)) and ((information[len(information) - 2:] == "pm") or (information[len(information) - 2:] == "am")):
+                    date.append(formattedDate)
+                    times.append(info[i])
+                    location.append((info[i + 1]))
+                    eventName.append((info[i + 2]))
+                    description.append((info[i + 3]))
+
+                else:
+                    continue
+            except ValueError:
+                pass
+
+    for i in range(477):
+         emptyDesc.append(" ")
+         emptyUpvotes.append("0")
+    print(len(eventName))
+    print(len(times))
+    print(len(location))
+    print(len(description))
+    print(len(date))
+    print(len(emptyUpvotes))
+    df = pd.DataFrame({'Name': eventName, 'Location': location, 'time': times, 'date': date, 'description': description,
+                       'upvotes': emptyUpvotes})
+    #df.to_csv('UrbanaEvents.csv', index=False, encoding='utf-8')
+
+    #Champaign event processing
+
+    driver = webdriver.Chrome()
+    eventName = []
+    date = []
+    times = []
+    location = []
+    info = []
+    contactInfo = []
     content = driver.get("https://www.visitchampaigncounty.org/events/calendar-of-events")
     info = (driver.find_elements_by_class_name('event-info'))
     driver.find_element_by_class_name("accept-cookies").click()
@@ -37,10 +98,6 @@ if __name__ == "__main__":
     for i in range(len(info)):
         emptyDesc.append(" ")
         emptyUpvotes.append("0")
-    df = pd.DataFrame({'Name': eventName, 'Location': location, 'time': times, 'date': date, 'description': emptyDesc, 'upvotes': emptyUpvotes})
+    df = pd.DataFrame({'Name': eventName, 'Location': location, 'time': times, 'date': date, 'description': emptyDesc,
+                       'upvotes': emptyUpvotes})
     df.to_csv('ChampaignEvents.csv', index=False, encoding='utf-8')
-
-    print(eventName)
-    print(date)
-
-
