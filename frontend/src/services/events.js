91ce2f659b42f138f9c898de731774f5c0fa95ea
George Pantazes <georgepantazes@gmail.com>
2019-09-28 09:44:25 -0500

    Frontend application work
    Add frontend beginnings

---
+++ frontend/src/services/events.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const allEvents = () => axios
+    .get("/events")
+    .then(response => response.data)
+    .catch(error => console.error(error))
+
+const getEvent = (id) => axios
+    .get(`/events/${id}`)
+    .then(response => response.data)
+    .catch(error => console.error(error))
+
+const updateEvent = (event) => axios
+    .put(`/events/${event.id}`, event)
+    .then(response => response.data)
+    .catch(error => console.error(error))
+
+const deleteEvent = (event) => axios
+    .delete(`/events/${event.id}`)
+    .then(response => response.data)
+    .catch(error => console.error(error))
+
+export default { allEvents, getEvent, updateEvent, deleteEvent }
